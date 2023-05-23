748da9ef74d6884d2e36e6932f730fc4ddd25dd9
manuel castillo chacon <castillomanuel955@gmail.com>
2023-05-22 22:43:57 -0500

    Revisar

--- DDL.sql
+++ DDL.sql
@@ -1,4 +1,147 @@
 -- TABLE Definition
 
+--122
+-- Constrainer
 
--- Constrains
+CREATE TABLE persona (
+
+    fecha_nacimiento DATE , 
+    apellido VARCHAR(25) ,
+    nombre  VARCHAR(25) ,
+    tipoDoc VARCHAR (15) ,
+    nrDoc VARCHAR(15)
+) ;
+
+CREATE TABLE tripulante (
+
+    cargo VARCHAR(20) , 
+    salario FLOAT ,
+    fecha_nacimiento DATE , 
+    apellido VARCHAR(25) ,
+    nombre  VARCHAR(25) ,
+    tipoDoc_persona VARCHAR (15) ,
+    nrDoc_persona VARCHAR(15)
+);
+
+CREATE TABLE comprador (
+
+    correo_electronico VARCHAR(40) , 
+    contasena VARCHAR(30) ,
+    fecha_nacimiento DATE , 
+    apellido VARCHAR(25) ,
+    nombre  VARCHAR(25) ,
+    tipoDoc_persona VARCHAR (15) ,
+    nrDoc_persona VARCHAR(15)    
+);
+
+CREATE TABLE pasajero (
+
+    fecha_nacimiento DATE , 
+    apellido VARCHAR(25) ,
+    nombre  VARCHAR(25) ,
+    tipoDoc_persona VARCHAR (15) ,
+    nrDoc_persona VARCHAR(15)
+) ;
+
+CREATE TABLE reserva (
+
+    fecha DATE ,
+    costo_total FLOAT,
+    codigo VARCHAR(10), 
+    tipoDoc_comprador VARCHAR (15) ,
+    nrDoc_comprador VARCHAR(15)
+) ; 
+
+CREATE TABLE reserva_pasajero(
+
+    id VARCHAR(15) , 
+    estado_check BIT ,
+    codigo VARCHAR(10) ,
+    tipoDoc_comprador VARCHAR (15) ,
+    nrDoc_comprador VARCHAR(15),
+    tipoDoc_pasajero VARCHAR (15) ,
+    nrDoc_pasajero VARCHAR(15)
+);
+
+CREATE TABLE poliza_de_seguro (
+
+    costo FLOAT ,
+    nroPoliza VARCHAR(12) ,
+    monto_cobertura FLOAT , 
+    fecha_inicial DATE ,
+    fecha_final DATE ,
+    id_reserva_pasajero VARCHAR(15) 
+) ; 
+
+CREATE TABLE equipaje (
+
+    peso_real FLOAT , 
+    tipo VARCHAR(15) , 
+    peso_maximo FLOAT , 
+    id  VARCHAR(15) , 
+    costo FLOAT ,
+    id_reserva_pasajero VARCHAR(15) ,
+    codigo_reserva VARCHAR(10) , 
+    tipoDoc_pasajero VARCHAR (15) ,
+    nrDoc_pasajero VARCHAR(15) ,
+    tipoDoc_comprador VARCHAR (15) ,
+    nrDoc_comprador VARCHAR(15)
+);
+
+CREATE TABLE asiento (
+
+    nroAsiento VARCHAR(15) ,
+    nroMatricula_avion VARCHAR(15)
+);
+
+CREATE TABLE avion (
+
+    nroMatricula VARCHAR(15) ,
+    capacidad INTEGER , 
+    nroVuelo_vuelo VARCHAR(10) 
+);
+
+CREATE TABLE vuelo (
+
+    fecha_salida DATE ,
+    fecha_llegada DATE ,
+    nroVuelo VARCHAR(10) ,
+    codigoIata_aeropuerto VARCHAR(15)
+);
+
+CREATE TABLE aeropuerto (
+
+    nombre VARCHAR(25),
+    ubicacion VARCHAR(50) , 
+    capacidad INTEGER ,
+    codigoIata VARCHAR(15) 
+);
+
+CREATE TABLE tripulante_asignado (
+    tipoDoc_persona_tripulante VARCHAR (15) ,
+    nrDoc_persona_tripulante VARCHAR(15) ,
+    nroVuelo_vuelo VARCHAR(10) 
+);
+
+CREATE TABLE incluye (
+
+    tipoDoc_pasajero VARCHAR (15) ,
+    nrDoc_pasajero VARCHAR(15) ,
+    tipoDoc_comprador VARCHAR (15) ,
+    nrDoc_comprador VARCHAR(15) ,
+    codigo_reserva VARCHAR(10) , 
+    id_reserva_pasajero VARCHAR(15) , 
+    nroVuelo_vuelo VARCHAR(10) 
+)
+
+CREATE TABLE asiento_ocupa(
+
+    id_reserva_pasajero VARCHAR(15) , 
+    nroMatricula_avion VARCHAR(15) , 
+    nroAsiento_asiento VARCHAR(15) ,
+    tipoDoc_pasajero VARCHAR (15) ,
+    nrDoc_pasajero VARCHAR(15) ,
+    codigo_reserva VARCHAR(10) , 
+    tipoDoc_comprador VARCHAR (15) ,
+    nrDoc_comprador VARCHAR(15) ,
+)
