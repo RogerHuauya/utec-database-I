85386234ba596bad7ab571620763b26cfb0ffb90
roger <rhuauyam@uni.pe>
2023-06-10 15:52:22 -0500

    finished development, to be tested

--- DDL.sql
+++ DDL.sql
@@ -116,5 +116,153 @@ CREATE TABLE pertenece (
 );
 
 
+-- Constrainer
 
+--Persona
+ALTER TABLE persona
+ADD CONSTRAINT pk_persona
+PRIMARY KEY (tipo_documento, nro_documento);
+
+--tripulante
+ALTER TABLE tripulante
+ADD CONSTRAINT fk_tripulante_persona
+FOREIGN KEY (tipo_documento_persona,nro_documento_persona) REFERENCES persona (tipo_documento, nro_documento);
+
+ALTER TABLE tripulante
+ADD CONSTRAINT pk_tripulante
+PRIMARY KEY (tipo_documento, nro_documento);
+
+--comprador
+Alter TABLE comprador
+ADD CONSTRAINT fk_comprador_persona
+FOREIGN KEY (tipo_documento_persona,nro_documento_persona) REFERENCES persona(tipo_documento, nro_documento);
+
+ALTER TABLE comprador
+ADD CONSTRAINT pk_comprador
+PRIMARY KEY (tipo_documento, nro_documento);
+
+--pasajero
+Alter TABLE pasajero
+ADD CONSTRAINT fk_pasajero_persona
+FOREIGN KEY (tipo_documento_persona, nro_documento_persona) REFERENCES persona(tipo_documento, nro_documento);
+
+ALTER TABLE pasajero
+ADD CONSTRAINT pk_pasajero
+PRIMARY KEY (tipo_documento, nro_documento);
+
+--reservacion
+ALTER TABLE reservacion
+ADD CONSTRAINT pk_reservacion
+PRIMARY KEY (codigo);
+
+ALTER TABLE reservacion 
+ADD CONSTRAINT fk_reservacion_comprador
+FOREIGN KEY (tipo_documento_comprador, nro_documento_comprador) REFERENCES comprador (tipo_documento, nro_documento)
+
+--subreservacion
+
+ALTER TABLE sub_reservacion
+ADD CONSTRAINT pk_sub_reservacion
+PRIMARY KEY(sub_reservacion_id);
+
+ALTER TABLE sub_reservacion
+ADD CONSTRAINT fk_sub_reservacion_pasajero
+FOREIGN KEY (tipo_documento_pasajero, nro_documento_pasajero) REFERENCES pasajero (tipo_documento, nro_documento);
+
+ALTER TABLE sub_reservacion 
+ADD CONSTRAINT fk_sub_reservacion_reservacion
+FOREIGN KEY (codigo_reservacion) REFERENCES reservacion (codigo);
+
+--sub_reserva_equipaje
+ALTER TABLE sub_reserva_equipaje
+add CONSTRAINT pk_sub_reserva_equipaje
+PRIMARY KEY (codigo_tipo_tipo_equipaje, sub_reservacion_id_sub_reservacion);
+
+Alter table sub_reserva_equipaje
+ADD CONSTRAINT fk_id_sub_reserva
+FOREIGN KEY(sub_reservacion_id_sub_reservacion)
+REFERENCES sub_reservacion(sub_reservacion_id);
+
+ALTER TABLE sub_reserva_equipaje
+ADD CONSTRAINT fk_sub_reserva_equipaje
+FOREIGN KEY (codigo_tipo_tipo_equipaje) REFERENCES tipo_equipaje (codigo_tipo);
+
+--tipo_equipaje
+ALTER TABLE tipo_equipaje
+ADD CONSTRAINT pk_tipo_equipaje
+PRIMARY KEY(codigo_tipo);
+
+--vuelo
+ALTER TABLE vuelo
+ADD CONSTRAINT pk_vuelo
+PRIMARY KEY (nro_vuelo);
+
+ALTER TABLE vuelo
+ADD CONSTRAINT fk_avion
+FOREIGN KEY (nro_matricula_avion)
+REFERENCES avion(nro_matricula)
+
+ALTER TABLE vuelo
+ADD CONSTRAINT fk_aeropuerto_origen
+FOREIGN KEY(codigo_iata_aeropuerto_origen)
+REFERENCES aeropuerto(codigo_iata)
+
+ALTER TABLE vuelo
+ADD CONSTRAINT fk_aeropuerto_destino
+FOREIGN KEY(codigo_iata_aeropuerto_destino)
+REFERENCES aeropuerto(codigo_iata)
+
+--tripulante_asignado
+ALTER TABLE tripulante_asignado
+ADD CONSTRAINT fk_tripulante_asignado
+FOREIGN KEY (tipo_documento_persona_tripulante, nro_documento_persona_tripulante)
+REFERENCES tripulante(tipo_documento_persona, nro_documento_persona)
+
+ALTER TABLE tripulante_asignado
+ADD CONSTRAINT fk_tripulante_asignado_vuelo
+FOREIGN KEY (nro_vuelo_vuelo)
+REFERENCES vuelo(nro_vuelo);
+
+ALTER TABLE tripulante_asignado
+ADD CONSTRAINT pk_tripulante_asignado
+PRIMARY KEY (tipo_documento_persona_tripulante, nro_documento_persona_tripulante, nro_vuelo_vuelo);
+
+--aeropuerto
+ALTER TABLE aeropuerto
+ADD CONSTRAINT pk_aeropuerto
+PRIMARY KEY (codigo_iata);
+
+--avion
+ALTER TABLE avion
+ADD CONSTRAINT pk_avion
+PRIMARY KEY (nro_matricula);
+
+--asiento_disponible
+ADD CONSTRAINT fk_asiento_disponible
+FOREIGN KEY (nro_vuelo_vuelo)
+REFERENCES vuelo(nro_vuelo);
+
+ALTER TABLE asiento_disponible
+ADD CONSTRAINT pk_asiento
+PRIMARY KEY (nro_asiento, nro_vuelo_vuelo);
+
+--pertenece
+ALTER TABLE pertenece
+ADD CONSTRAINT fk_pertenece_asientodis
+FOREIGN KEY (nro_asiento_asiento_disponible)
+REFERENCES asiento_disponible(nro_asiento);
+
+ALTER TABLE pertenece
+ADD CONSTRAINT fk_pertenece_vuelo
+FOREIGN KEY (nro_vuelo_vuelo)
+REFERENCES vuelo (nro_vuelo);
+
+ALTER TABLE pertenece
+ADD CONSTRAINT fk_pertenece_subreserva
+FOREIGN KEY (sub_reservacion_id_sub_reservacion)
+REFERENCES sub_reservacion(sub_reservacion_id);
+
+ALTER TABLE pertenece
+ADD CONSTRAINT pk_pertenece
+PRIMARY KEY (nro_asiento_asiento_disponible, nro_vuelo_vuelo);
 
