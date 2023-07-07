205460ed81e276c694c11c883f48423d02a90a7e
manuel castillo chacon <castillomanuel955@gmail.com>
2023-07-06 20:22:17 -0500

    as

--- project/query2_index.sql
+++ project/query2_index.sql
@@ -1,8 +1,8 @@
--- CREATE INDEX
-CREATE INDEX avionNroMatriculaIndex ON avion USING hash (nro_matricula);
+CREATE INDEX vueloFechaSalidaIndex ON vuelo USING btree (fecha_salida);
 CREATE INDEX vueloNroMatriculaAvionIndex ON vuelo USING hash (nro_matricula_avion);
 CREATE INDEX asientoDisponibleNroVueloVueloIndex ON asiento_disponible USING hash (nro_vuelo_vuelo);
-CREATE INDEX perteneceIndex ON pertenece USING hash (nro_asiento_asiento_disponible, nro_vuelo_vuelo);
-CREATE INDEX subReservacionIndex ON sub_reservacion USING hash (sub_reservacion_id);
-CREATE INDEX reservacionCodigoIndex ON reservacion USING hash (codigo);
-CREATE INDEX vueloFechaSalidaIndex ON vuelo USING btree (fecha_salida);
+CREATE INDEX perteneceAsientoIndex ON pertenece USING hash (nro_asiento_asiento_disponible) ;
+CREATE INDEX perteneceVueloIndex ON pertenece USING hash ( nro_vuelo_vuelo) ;
+CREATE INDEX perteneceReservacionIndex ON pertenece USING hash (sub_reservacion_id_sub_reservacion);
+
+--esasd
