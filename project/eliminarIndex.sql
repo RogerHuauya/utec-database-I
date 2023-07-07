a13da47a2bd5d21d2a29e4a0eda12ae8d7fdff13
Lenovo <castillomanuel955@gmail.com>
2023-07-06 19:41:16 -0500

    asa

---
+++ project/eliminarIndex.sql
@@ -0,0 +1,27 @@
+-- CREATE INDEX query 1
+DROP INDEX personaNroDocumentoIndex;
+DROP INDEX pasajeroNroDocumentoPersonaIndex;
+DROP INDEX subReservacionNroDocumentoPasajeroIndex ;
+DROP  INDEX reservacionCodigoFechaIndex;
+DROP INDEX subReservaEquipajeSubReservacionIdIndex ;
+DROP INDEX tipoEquipajeCodigoTipoIndex;
+DROP INDEX perteneceSubReservacionIdIndex ;
+DROP INDEX vueloNroVueloIndex ;
+DROP INDEX tripulanteAsignadoNroVueloVueloIndex ;
+DROP INDEX tripulanteNroDocumentoPersonaIndex ;
+DROP INDEX aeropuertoCodigoIataIndex ;
+DROP INDEX aeropuertoCapacidadIndex ;
+
+
+DROP INDEX avionNroMatriculaIndex ;
+DROP  INDEX vueloNroMatriculaAvionIndex ;
+DROP INDEX asientoDisponibleNroVueloVueloIndex;
+DROP INDEX perteneceIndex ;
+DROP  INDEX subReservacionIndex ;
+DROP INDEX reservacionCodigoIndex ;
+DROP INDEX vueloFechaSalidaIndex ;
+
+DROP INDEX subIdIndex ;
+DROP INDEX subReservacionIdIndex;
+DROP  INDEX vueloFsIndex  ;
+DROP INDEX pasajeroNrDocIndex;
